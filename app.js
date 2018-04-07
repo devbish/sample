a7c8074803d803d2fb249266cb90ad8b967b00d4
Bishal Dey <deybishal2k15@gmail.com>
2018-04-07 12:37:19 +0530

    new Changes

---
+++ app.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById('btn-new');
+var cont = document.getElementById('content');
+
+btn.addEventListener('click', function (){
+    console.log('Hello!');
+    cont.innerHTML = "This is a Text!";
+});
+
+
